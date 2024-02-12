e1ecaa828130972748012625d94858ee03e57026
so1yana <trapbigest@gmail.com>
2024-02-12 19:41:35 +0300

    actions were separated

---
+++ src/actions/filters.js
@@ -0,0 +1,5 @@
+export const all = (value) => ({ type: 'FILTER', filter: 'ALL', value });
+export const without = (value) => ({ type: 'FILTER', filter: 'WITHOUT', value });
+export const one = (value) => ({ type: 'FILTER', filter: 'ONE', value });
+export const two = (value) => ({ type: 'FILTER', filter: 'TWO', value });
+export const three = (value) => ({ type: 'FILTER', filter: 'THREE', value });
---
+++ src/actions/sorting.js
@@ -0,0 +1,3 @@
+export const cheap = () => ({ type: 'SORTING', sorting: 'CHEAP' });
+export const fast = () => ({ type: 'SORTING', sorting: 'FAST' });
+export const optimal = () => ({ type: 'SORTING', sorting: 'OPTIMAL' });
