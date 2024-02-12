d84d6aaee5bb358a51e1bd6f30195909868c9751
so1yana <trapbigest@gmail.com>
2024-02-12 19:40:25 +0300

    refactoring for main index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import reducer from './reducer';
 import App from './components/app';
 import './index.scss';
 import './reset.css';
 
+const store = configureStore({ reducer });
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <Provider store={store}>
         <App />
-    </React.StrictMode>,
+    </Provider>,
 );
+
+// const { isAllActive, ...left } = store.getState();
+// const isAllChecked = Object.entries(left).every((el) => el[1]);
+// if (isAllChecked && !isAllActive) store.dispatch({ type: 'ALL', value: true });
