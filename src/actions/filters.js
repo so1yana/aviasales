a9b89e0a2dc83a8f1cb89f730c109c9a71cff102
so1yana <trapbigest@gmail.com>
2024-02-13 21:22:56 +0300

    added a temp action to add a card

--- src/actions/filters.js
+++ src/actions/filters.js
@@ -3,3 +3,13 @@ export const without = (value) => ({ type: 'FILTER', filter: 'WITHOUT', value })
 export const one = (value) => ({ type: 'FILTER', filter: 'ONE', value });
 export const two = (value) => ({ type: 'FILTER', filter: 'TWO', value });
 export const three = (value) => ({ type: 'FILTER', filter: 'THREE', value });
+// export const add = (cards) => {
+//     return { type: 'CARD', card: 'ADD', body: cards };
+// };
+export const add = (cards) => {
+    return (dispatch) => {
+        setTimeout(() => {
+            dispatch({ type: 'CARD', card: 'ADD', body: cards });
+        }, 2000);
+    };
+};
