f166081bef295114b5711ae00176cbfac60fba90
so1yana <trapbigest@gmail.com>
2024-02-17 21:12:47 +0300

    added logic of formating data

--- src/components/ticket-item/ticket-item.js
+++ src/components/ticket-item/ticket-item.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import './ticket-item.scss';
 
 const calculateTime = (minutes) => {
@@ -7,22 +8,40 @@ const calculateTime = (minutes) => {
     return `${hours}ч ${remainingMinutes}м`;
 };
 
+const toNormalTime = (initDate, duration) => {
+    const firstDate = new Date(initDate);
+    const secDate = new Date(duration * 60 * 1000 + firstDate.getTime());
+    let firstHours = firstDate.getHours();
+    let firstMinutes = firstDate.getMinutes();
+    let secondHours = secDate.getHours();
+    let secondMinutes = secDate.getMinutes();
+    if (firstHours < 10) firstHours = `0${firstHours}`;
+    if (firstMinutes < 10) firstMinutes = `0${firstMinutes}`;
+    if (secondHours < 10) secondHours = `0${secondHours}`;
+    if (secondMinutes < 10) secondMinutes = `0${secondMinutes}`;
+    return `${firstHours}:${firstMinutes} - ${secondHours}:${secondMinutes}`;
+};
+
 export default function TicketItem(props) {
     const { ticket } = props;
-    const { price, carrier, segments, display } = ticket;
-    if (!display) return null;
+    const { price, carrier, segments } = ticket;
     const [firstSeg, secSeg] = segments;
     const firstCities = `${firstSeg.origin}-${firstSeg.destination}`;
     const secondCities = `${secSeg.origin}-${secSeg.destination}`;
     const firstTime = calculateTime(firstSeg.duration);
     const secondTime = calculateTime(secSeg.duration);
-
+    const firstSegTime = toNormalTime(new Date(firstSeg.date).getTime(), firstSeg.duration);
+    const secSegTime = toNormalTime(new Date(secSeg.date).getTime(), secSeg.duration);
     return (
         <li className="ticket">
             <div className="ticket__body">
                 <div className="ticket__header">
                     <p className="ticket__price">{price} ₽</p>
-                    <p className="ticket__company">{carrier}</p>
+                    <img
+                        className="ticket__company"
+                        alt="logo"
+                        src={`//pics.avs.io/99/36/${carrier}.png`}
+                    />
                 </div>
                 <div className="ticket__info">
                     <table className="ticket-table">
@@ -33,7 +52,7 @@ export default function TicketItem(props) {
                                 <th className="ticket-table__header">Пересадки</th>
                             </tr>
                             <tr className="ticket-table__row">
-                                <td className="ticket-table__data">{firstSeg.date}</td>
+                                <td className="ticket-table__data">{firstSegTime}</td>
                                 <td className="ticket-table__data">{firstTime}</td>
                                 <td className="ticket-table__data">{firstSeg.stops.join(', ')}</td>
                             </tr>
@@ -43,7 +62,7 @@ export default function TicketItem(props) {
                                 <th className="ticket-table__header">Пересадки</th>
                             </tr>
                             <tr className="ticket-table__row">
-                                <td className="ticket-table__data">{secSeg.date}</td>
+                                <td className="ticket-table__data">{secSegTime}</td>
                                 <td className="ticket-table__data">{secondTime}</td>
                                 <td className="ticket-table__data">{secSeg.stops.join(', ')}</td>
                             </tr>
