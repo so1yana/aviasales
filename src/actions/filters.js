13259bc3e4579d81eb6018fbe37e93694a9196c2
so1yana <trapbigest@gmail.com>
2024-02-17 21:07:38 +0300

    reworked actions with filters

--- src/actions/filters.js
+++ src/actions/filters.js
@@ -1,15 +1,47 @@
-export const all = (value) => ({ type: 'FILTER', filter: 'ALL', value });
-export const without = (value) => ({ type: 'FILTER', filter: 'WITHOUT', value });
-export const one = (value) => ({ type: 'FILTER', filter: 'ONE', value });
-export const two = (value) => ({ type: 'FILTER', filter: 'TWO', value });
-export const three = (value) => ({ type: 'FILTER', filter: 'THREE', value });
-// export const add = (cards) => {
-//     return { type: 'CARD', card: 'ADD', body: cards };
-// };
-export const add = (cards) => {
+import { filter, addOnShow, cleanOnShow, sort } from './cards';
+
+export const all = (value) => {
     return (dispatch) => {
-        setTimeout(() => {
-            dispatch({ type: 'CARD', card: 'ADD', body: cards });
-        }, 2000);
+        dispatch({ type: 'FILTER', filter: 'ALL', value });
+        dispatch(filter());
+        dispatch(sort());
+        dispatch(cleanOnShow());
+        dispatch(addOnShow());
+    };
+};
+export const without = (value) => {
+    return (dispatch) => {
+        dispatch({ type: 'FILTER', filter: 'WITHOUT', value });
+        dispatch(filter());
+        dispatch(sort());
+        dispatch(cleanOnShow());
+        dispatch(addOnShow());
+    };
+};
+export const one = (value) => {
+    return (dispatch) => {
+        dispatch({ type: 'FILTER', filter: 'ONE', value });
+        dispatch(filter());
+        dispatch(sort());
+        dispatch(cleanOnShow());
+        dispatch(addOnShow());
+    };
+};
+export const two = (value) => {
+    return (dispatch) => {
+        dispatch({ type: 'FILTER', filter: 'TWO', value });
+        dispatch(filter());
+        dispatch(sort());
+        dispatch(cleanOnShow());
+        dispatch(addOnShow());
+    };
+};
+export const three = (value) => {
+    return (dispatch) => {
+        dispatch({ type: 'FILTER', filter: 'THREE', value });
+        dispatch(filter());
+        dispatch(sort());
+        dispatch(cleanOnShow());
+        dispatch(addOnShow());
     };
 };
