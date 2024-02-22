9654683ce7d938fba18ceb844e037b45277c5572
so1yana <trapbigest@gmail.com>
2024-02-22 09:09:57 +0300

    removed unused actions

--- src/actions/sorting.js
+++ src/actions/sorting.js
@@ -1,26 +1,20 @@
-import { sort, addOnShow, cleanOnShow } from './cards';
+import { sort } from './cards';
 
 export const cheap = () => {
     return (dispatch) => {
         dispatch({ type: 'SORTING', sorting: 'CHEAP' });
         dispatch(sort());
-        dispatch(cleanOnShow());
-        dispatch(addOnShow());
     };
 };
 export const fast = () => {
     return (dispatch) => {
         dispatch({ type: 'SORTING', sorting: 'FAST' });
         dispatch(sort());
-        dispatch(cleanOnShow());
-        dispatch(addOnShow());
     };
 };
 export const optimal = () => {
     return (dispatch) => {
         dispatch({ type: 'SORTING', sorting: 'OPTIMAL' });
         dispatch(sort());
-        dispatch(cleanOnShow());
-        dispatch(addOnShow());
     };
 };
