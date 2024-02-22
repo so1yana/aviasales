8b7a602cb07fbf40eff90f82389fd49bceb57f0d
so1yana <trapbigest@gmail.com>
2024-02-22 09:14:09 +0300

    removed circular dependencies

--- src/ticketsApi/index.js
+++ src/ticketsApi/index.js
@@ -1,11 +1,11 @@
-// eslint-disable-next-line import/no-cycle
-import { add, filter, addOnShow } from '../actions/cards';
+import { add, addOnShow, sort } from '../actions/cards';
+import { error, loaded } from '../actions/status';
 
 async function getSearchId() {
     let searchId = null;
     const result = await fetch('https://aviasales-test-api.kata.academy/search')
         .then((response) => response.json())
-        .catch((error) => ({ message: error.message }));
+        .catch((err) => ({ message: err.message }));
 
     if (!Object.prototype.hasOwnProperty.call(result, 'searchId')) searchId = await getSearchId();
     else searchId = result.searchId;
@@ -13,13 +13,35 @@ async function getSearchId() {
 }
 
 let responsesCount = 0;
+let errorsCount = 0;
+let returnMessage = 'OK';
 async function getPackTickets(searchId, dispatch) {
     const response = await fetch(
         `https://aviasales-test-api.kata.academy/tickets?searchId=${searchId}`,
     );
+    if (errorsCount >= 3) {
+        dispatch(error());
+        returnMessage = 'ERROR';
+        errorsCount = 0;
+        return returnMessage;
+    }
     if (response.status === 502) {
+        errorsCount += 1;
+        if (errorsCount >= 3) {
+            dispatch(error());
+            returnMessage = 'ERROR';
+            errorsCount = 0;
+            return returnMessage;
+        }
         await getPackTickets(searchId, dispatch);
     } else if (response.status !== 200) {
+        errorsCount += 1;
+        if (errorsCount >= 3) {
+            dispatch(error());
+            returnMessage = 'ERROR';
+            errorsCount = 0;
+            return returnMessage;
+        }
         await new Promise((resolve) => {
             setTimeout(resolve, 1000);
         });
@@ -27,12 +49,18 @@ async function getPackTickets(searchId, dispatch) {
     } else {
         responsesCount += 1;
         const json = await response.json();
-        await dispatch(add(json.tickets));
-        await dispatch(filter(json.tickets));
-        if (responsesCount === 1) await dispatch(addOnShow());
-        if (json.stop) return;
+        dispatch(add(json.tickets));
+        dispatch(sort());
+        if (responsesCount === 1) dispatch(addOnShow(5));
+        if (json.stop) {
+            dispatch(loaded());
+            returnMessage = 'OK';
+            errorsCount = 0;
+            return returnMessage;
+        }
         await getPackTickets(searchId, dispatch);
     }
+    return returnMessage;
 }
 
 export { getPackTickets, getSearchId };
