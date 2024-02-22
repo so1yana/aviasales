7815ecbc85607c8cadb97362f7cb7c9a32766186
so1yana <trapbigest@gmail.com>
2024-02-22 09:09:40 +0300

    removed unused actions

--- src/actions/filters.js
+++ src/actions/filters.js
@@ -1,47 +1,32 @@
-import { filter, addOnShow, cleanOnShow, sort } from './cards';
+import { addOnShow } from './cards';
 
 export const all = (value) => {
     return (dispatch) => {
         dispatch({ type: 'FILTER', filter: 'ALL', value });
-        dispatch(filter());
-        dispatch(sort());
-        dispatch(cleanOnShow());
-        dispatch(addOnShow());
+        dispatch(addOnShow(5));
     };
 };
 export const without = (value) => {
     return (dispatch) => {
         dispatch({ type: 'FILTER', filter: 'WITHOUT', value });
-        dispatch(filter());
-        dispatch(sort());
-        dispatch(cleanOnShow());
-        dispatch(addOnShow());
+        dispatch(addOnShow(5));
     };
 };
 export const one = (value) => {
     return (dispatch) => {
         dispatch({ type: 'FILTER', filter: 'ONE', value });
-        dispatch(filter());
-        dispatch(sort());
-        dispatch(cleanOnShow());
-        dispatch(addOnShow());
+        dispatch(addOnShow(5));
     };
 };
 export const two = (value) => {
     return (dispatch) => {
         dispatch({ type: 'FILTER', filter: 'TWO', value });
-        dispatch(filter());
-        dispatch(sort());
-        dispatch(cleanOnShow());
-        dispatch(addOnShow());
+        dispatch(addOnShow(5));
     };
 };
 export const three = (value) => {
     return (dispatch) => {
         dispatch({ type: 'FILTER', filter: 'THREE', value });
-        dispatch(filter());
-        dispatch(sort());
-        dispatch(cleanOnShow());
-        dispatch(addOnShow());
+        dispatch(addOnShow(5));
     };
 };
