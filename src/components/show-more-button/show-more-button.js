985063dd378e158ae8564cb3e4d1b648329a3a0a
so1yana <trapbigest@gmail.com>
2024-02-22 09:12:11 +0300

    removed unused state prop and reworked render logic

--- src/components/show-more-button/show-more-button.js
+++ src/components/show-more-button/show-more-button.js
@@ -1,20 +1,21 @@
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addOnShow } from '../../actions/cards';
 import classes from './show-more-button.module.scss';
 
-function ShowMoreButton({ state, add }) {
-    const activeCards = state.cardsOnShow.length;
-    if (activeCards > 4)
+function ShowMoreButton() {
+    const dispatch = useDispatch();
+    const activeCards = useSelector((state) => state.cardsOnShow);
+    if (activeCards >= 5)
         return (
-            <button className={classes['show-more-items-button']} type="button" onClick={add}>
+            <button
+                className={classes['show-more-items-button']}
+                type="button"
+                onClick={() => dispatch(addOnShow(activeCards + 5))}
+            >
                 показать еще
             </button>
         );
     return null;
 }
 
-const mapStateToProps = (state) => {
-    return { state };
-};
-
-export default connect(mapStateToProps, { add: addOnShow })(ShowMoreButton);
+export default ShowMoreButton;
